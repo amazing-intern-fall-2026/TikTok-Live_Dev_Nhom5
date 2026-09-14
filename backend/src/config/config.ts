34d171afc45dfaa3380aa0c5d5c9b47cf25629c8
patohru <patohru2006@gmail.com>
2026-09-14 20:00:29 +0700

    feat: setup API key for tiktok connector

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,11 +4,17 @@ dotenv.config();
 
 interface Env {
     PORT: number;
+    EULER_API_KEY: string;
+}
+
+if (!process.env.EULER_API_KEY) {
+    throw new Error("EULER_API_KEY is not defined");
 }
 
 const config = (): Env => {
     return {
         PORT: process.env.PORT ? Number(process.env.PORT) : 9090,
+        EULER_API_KEY: String(process.env.EULER_API_KEY),
     };
 };
 

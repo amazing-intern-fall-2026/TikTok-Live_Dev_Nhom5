fc29b69397e127c4a41bacd8aa509d40bddd2a57
WhiteDev <151415090+White2908@users.noreply.github.com>
2026-09-14 16:35:54 +0700

    feat:remove hard code

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://horse-racing-api.patohru.qzz.io/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {

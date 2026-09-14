6fedd5dd4de91753049683913a52ffa9a82ea5a2
patohru <patohru2006@gmail.com>
2026-09-14 12:11:22 +0700

    feat: setup socketio with bun engine
    This handle CHAT event for testing and not for actual use yet

---
+++ backend/src/sockets/index.ts
@@ -0,0 +1,60 @@
+import { Server as Engine } from "@socket.io/bun-engine";
+import { Server } from "socket.io";
+import { TikTokConnectionWrapper } from "../services/tiktok";
+import { WebcastEvent } from "tiktok-live-connector";
+
+let io: Server | undefined;
+
+export function initializeSockets(bunEngine: Engine) {
+    io = new Server({
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+        },
+    });
+    io.bind(bunEngine);
+    io.on("connection", (socket) => {
+        let tikTokConnectionWrapper;
+        console.log(`[Socket] Client connected ${socket.id}`);
+
+        socket.on("disconnect", (reason) => {
+            console.log(`Client disconnected: ${socket.id} (${reason})`);
+        });
+
+        socket.on("setUniqueID", async (uniqueID, options) => {
+            console.log(`[Socket] ${socket.id} got ${uniqueID}`);
+
+            try {
+                tikTokConnectionWrapper = new TikTokConnectionWrapper(
+                    uniqueID,
+                    options,
+                    true,
+                );
+                tikTokConnectionWrapper.connect();
+            } catch (err: any) {
+                socket.emit("tiktokDisconnected", err.toString());
+                return;
+            }
+
+            tikTokConnectionWrapper.once("connected", (state) =>
+                socket.emit("tiktokConnected", state),
+            );
+            tikTokConnectionWrapper.once("disconnected", (reason) =>
+                socket.emit("tiktokDisconnected", reason),
+            );
+
+            tikTokConnectionWrapper.connection.on(WebcastEvent.CHAT, (msg) => {
+                console.log(msg.content);
+                socket.emit("chat", msg);
+            });
+        });
+    });
+}
+
+export function getIO() {
+    if (!io) {
+        throw new Error("Socket.IO has not been initialized.");
+    }
+
+    return io;
+}
